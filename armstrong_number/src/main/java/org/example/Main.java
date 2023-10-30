a9175bb75739db6566190fc1919ba37ff5c25fe5
Fatih <mfu97@hotmail.com>
2023-10-30 14:40:25 +0300

    firs commit

--- armstrong_number/src/main/java/org/example/Main.java
+++ armstrong_number/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (isArmstrongNumber(number)) {
+            System.out.println(number + " bir Armstrong sayısıdır.");
+        } else {
+            System.out.println(number + " bir Armstrong sayısı değildir.");
+        }
     }
+
+    // Bir sayının Armstrong sayısı olup olmadığını kontrol eden fonksiyon
+    static boolean isArmstrongNumber(int Number) {
+        int toplam = 0;
+        int tempNum = Number;
+        int numOfDigits = String.valueOf(Number).length();
+
+        while (tempNum != 0) {
+            int digit = tempNum % 10;
+            toplam += Math.pow(digit, numOfDigits);
+            tempNum /= 10;
+        }
+
+        return toplam == Number;
+    }
+
 }
