3e6a48aa5f9fa5930f0e79bc72163c4e4bb80ae9
Ege Kavcıoğlu <sonicster1458@hotmail.com>
2023-10-30 16:55:48 -0200

    text commit

---
+++ give_string_char/src/main/java/org/example/CharacterCounts.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class CharacterCounts {
+    public static void main(String[] args) {
+        String input = "Tobeto";
+        countAndPrintCharacterCounts(input);
+    }
+
+    public static void countAndPrintCharacterCounts(String input) {
+        // Karakter sayımlarını depolamak için bir HashMap oluştur
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+
+        // Giriş dizesini küçük harfe dönüştür
+        input = input.toLowerCase();
+
+        // Dizedeki karakterler arasında yineleme
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                // Only count letters, not other characters
+                charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        // Karakter sayılarını yazdır
+        for (char c : charCountMap.keySet()) {
+            System.out.println(c + ":" + charCountMap.get(c));
+        }
+    }
+}
+
