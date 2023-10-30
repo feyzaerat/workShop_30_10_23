748a87fc5c82e1896f933c745a326cc862255874
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-10-30 17:46:05 +0300

    created fibonacci series

--- fibonacci_series/src/main/java/org/example/Main.java
+++ fibonacci_series/src/main/java/org/example/Main.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 15;
+        int firstNumber = 0;
+        int secondNumber = 1;
+
+        System.out.println("There is Fibonacci series till " + n + ":" );
+
+        for(int i=1; i <= n; ++i){
+            System.out.print(firstNumber + ", ");
+            int nextNumber = firstNumber + secondNumber ;
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+        }
     }
 }
