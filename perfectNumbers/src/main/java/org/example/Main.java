bd5956d3f5b77e4ad1ce87dfd0760e9e9c40335e
Yagmur Curku <yagmurcurku@gmail.com>
2023-10-30 20:03:33 +0300

    created perfect numbers

--- perfectNumbers/src/main/java/org/example/Main.java
+++ perfectNumbers/src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int total = 0;
+        int number = 2;
+        while (total < 10) {
+
+            int sum = 0;
+            for(int i = 1; i < number ; i++) {
+                if(number % i == 0) {
+                    sum += i;
+                }
+            }
+            if(sum == number) {
+                total +=1 ;
+                System.out.println(number + " is the perfect number");
+            }
+
+            number ++;
+        }
+
     }
 }
