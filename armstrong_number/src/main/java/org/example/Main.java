7c0bcbaecfb0c3b4e4f9c27ff776378b8d0ab367
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-10-31 00:35:29 +0300

    added scanner

--- armstrong_number/src/main/java/org/example/Main.java
+++ armstrong_number/src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
     }
 }
