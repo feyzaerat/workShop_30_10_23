034e8841356c99582401675e58970105dcfdd8be
Fatih <mfu97@hotmail.com>
2023-10-30 15:44:26 +0300

    initial Fibonacci series

--- fibonacci_series/src/main/java/org/example/Main.java
+++ fibonacci_series/src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 100;
+        int[] fibonacciSeries = new int[n];
+
+        // İlk iki Fibonacci sayısı
+        fibonacciSeries[0] = 0;
+        fibonacciSeries[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            fibonacciSeries[i] = fibonacciSeries[i - 1] + fibonacciSeries[i - 2];
+        }
+
+        System.out.println("Fibonacci Serisinin ilk 100 Sayısı):");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacciSeries[i] + " ");
+        }
     }
 }
