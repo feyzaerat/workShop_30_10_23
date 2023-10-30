8dfc3c6248772f473e3d01b4099444675494f6dc
Yagmur Curku <yagmurcurku@gmail.com>
2023-10-30 15:58:53 +0300

    test commit

--- armstrong_number/src/main/java/org/example/Main.java
+++ armstrong_number/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        String numberStr = scanner.nextLine();
+
+        int numberLength = numberStr.length();
+        int numberNormal = Integer.parseInt(numberStr);
+
+        int number = numberNormal;
+        int total = 0;
+        int remainder = 0;
+
+        while(number != 0){
+            remainder = number % 10;
+            total += (int)Math.pow(remainder, numberLength);
+            number = number / 10;
+        }
+
+        if(total == numberNormal){
+            System.out.println(numberStr + " is an Armstrong number");
+        } else{
+            System.out.println(numberStr + " is not an Armstrong number");
+        }
+
+
     }
 }
