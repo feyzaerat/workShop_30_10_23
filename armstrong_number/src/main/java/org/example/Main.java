6b0185f16542b4bde8d89043d74d37802b6e6b59
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-10-31 01:00:22 +0300

    pulled from feyza

--- armstrong_number/src/main/java/org/example/Main.java
+++ armstrong_number/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int numberOfDigits = 0;
     }
 }
