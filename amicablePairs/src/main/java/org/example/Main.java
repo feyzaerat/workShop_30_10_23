a4ea74b39173c9cbcc4de59f1833aa250fec56c7
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-10-31 00:15:41 +0300

    created amicable pairs

--- amicablePairs/src/main/java/org/example/Main.java
+++ amicablePairs/src/main/java/org/example/Main.java
@@ -1,9 +1,38 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        //Numbers whose sum of all divisors other than themselves are equal to each other are called amicable pairs.
 
-        //te
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter first number :  ");
+        int firstNumber = input.nextInt();
+
+        System.out.println("Please enter second number :  ");
+        int secondNumber = input.nextInt();
+
+
+        int sum1 = 0;
+        int sum2 =0;
+
+        for(int i = 1; i < firstNumber; i++){
+            if(firstNumber % i == 0){
+                sum1 = sum1 + i;
+            }
+        }
+        for(int i = 1; i < secondNumber; i++){
+            if(secondNumber % i == 0){
+                sum2 = sum2 + i;
+            }
+        }
+        if (firstNumber == sum2 && secondNumber == sum1) {
+           System.out.println("These two numbers are amicable pairs..");
+        }else {
+            System.out.println("These two numbers are not amicable pairs..");
+        }
+
     }
 }
