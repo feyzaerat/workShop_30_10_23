e4479c35c12efc62ce521ecc75a163328e6f10b2
Fatih <mfu97@hotmail.com>
2023-11-01 02:23:14 +0300

    perfect number

--- perfectNumbers/src/main/java/org/example/Main.java
+++ perfectNumbers/src/main/java/org/example/Main.java
@@ -2,6 +2,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int sayi = 2;
+        int sayac = 0;
+
+        while (sayac < 10) {
+            if (isMukemmelSayi(sayi)) {
+                System.out.println(sayi);
+                sayac++;
+            }
+            sayi++;
+        }
+    }
+
+    // Bir sayının mükemmel sayı olup olmadığını kontrol eden fonksiyon
+    public static boolean isMukemmelSayi(int sayi) {
+        int toplam = 1; // 1 her zaman bölenlerin toplamının bir parçasıdır
+        for (int i = 2; i <= sayi / 2; i++) {
+            if (sayi % i == 0) {
+                toplam += i;
+            }
+        }
+        return toplam == sayi;
     }
 }
