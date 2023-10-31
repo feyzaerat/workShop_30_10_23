de7d19a27f456e093efeba1de64bed74ffe13b9e
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-10-31 03:03:55 +0300

    completed stringChar

--- give_string_char/src/main/java/org/example/Main.java
+++ give_string_char/src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("--------------------------------------------------------------------------------------------");
+        System.out.println("Please enter the word for which you want the number of letter repetitions to be calculated : ");
+        System.out.println("--------------------------------------------------------------------------------------------");
+
+        String message = input.next();
+        String result = "";
+        int counter = 0;
+        System.out.println("Total number of letters of " + message + " : " + message.length());
+
+        for (int i = 0; i < message.length(); i++) {
+            counter = 0;
+
+            for (int j = 0; j < message.length(); j++) {
+
+                if (message.charAt(i) == message.charAt(j)) {
+                    counter++;
+                }
+            }
+
+            if (!result.contains(message.substring(i, i + 1))) {
+                result += message.substring(i, i + 1) + "=> " + counter + " ,";
+            }
+        }
+
+        System.out.println("RESULT : " + result);
+
     }
 }
