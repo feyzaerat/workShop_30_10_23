6df764f83c538b48097d30138b8dbdad3da3a07b
Ege Kavcıoğlu <sonicster1458@hotmail.com>
2023-10-30 23:15:46 -0200

    created isAnagram

---
+++ isAnagram/src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class armstrongNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (isArmstrongNumber(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+
+    public static boolean isArmstrongNumber(int number) {
+        int originalNumber, remainder, result = 0;
+        int n = 0;
+
+        originalNumber = number;
+
+        // Sayıdaki basamak sayısını yaz
+        while (originalNumber != 0) {
+            originalNumber /= 10;
+            n++;
+        }
+
+        originalNumber = number;
+
+        // Rakamların n'inci kuvvetlerinin toplamını hesapla
+        while (originalNumber != 0) {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, n);
+            originalNumber /= 10;
+        }
+
+        // Sonucun orijinal sayıya eşit olup olmadığını kontrol et
+        return result == number;
+    }
+}
