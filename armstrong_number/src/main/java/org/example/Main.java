7f469f6506a443edb94ec2b12562127b61049166
Ege Kavcıoğlu <sonicster1458@hotmail.com>
2023-10-30 23:24:43 -0200

    updated armstrongNumberChecker.java file name

--- armstrong_number/src/main/java/org/example/Main.java
+++ armstrong_number/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class armstrongNumberChecker {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
