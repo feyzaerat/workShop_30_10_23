d9b68800b976e9a57b83ee86111d5f6c0a4c22d1
Ege Kavcıoğlu <sonicster1458@hotmail.com>
2023-10-30 23:25:33 -0200

    updated CharacterCounts.java file name

--- give_string_char/src/main/java/org/example/Main.java
+++ give_string_char/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.HashMap;
 
-public class CharacterCounts {
+public class Main {
     public static void main(String[] args) {
         String input = "Tobeto";
         countAndPrintCharacterCounts(input);
