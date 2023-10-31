bf3e34c10347b036fb7a6ef2373ac2a7824b7288
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-10-31 03:20:36 +0300

    created anagram

--- isAnagram/src/main/java/org/example/Main.java
+++ isAnagram/src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first word : ");
+        String firstWord = input.next();
+
+        System.out.println("Enter second word : ");
+        String secondWord = input.next();
+
+        Boolean result = true;
+
+
+        if (firstWord.length() == secondWord.length()) {
+            for (int i = 0; i < firstWord.length(); i++) {
+                if (firstWord.toLowerCase().contains((secondWord.toLowerCase().substring(i, i + 1)))) {
+                    continue;
+                } else {
+                    result = false;
+                    break;
+                }
+            }
+        } else {
+            result = false;
+        }
+        if (result) {
+            System.out.println("RESULT : " + firstWord + " & " + secondWord + " => ANAGRAM");
+        } else {
+            System.out.println("RESULT : These are not Anagram !! ");
+        }
     }
 }
