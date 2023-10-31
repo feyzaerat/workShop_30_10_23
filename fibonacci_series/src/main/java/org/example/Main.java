439fc7f7e45d0d02238b6699b333c8579a5595f7
Ege Kavcıoğlu <sonicster1458@hotmail.com>
2023-10-30 23:21:49 -0200

    created fibonacci series

--- fibonacci_series/src/main/java/org/example/Main.java
+++ fibonacci_series/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printFibonacciSeries(100);
+    }
+
+    public static void printFibonacciSeries(int n) {
+        long[] fibonacciNumbers = new long[n];
+        fibonacciNumbers[0] = 0;
+        fibonacciNumbers[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+        }
+
+        System.out.println("The first " + n + " numbers in the Fibonacci series are:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacciNumbers[i] + " ");
+        }
     }
 }
